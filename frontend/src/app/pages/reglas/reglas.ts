1f4735ff5d88f4936606d945ec7a80500e8e5950
IgnacioCantoni <ignaciomatiascantoni2001@gmail.com>
2026-06-11 10:44:43 -0300

    xd

--- frontend/src/app/pages/reglas/reglas.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-reglas',
-  imports: [],
-  templateUrl: './reglas.html',
-  styleUrl: './reglas.scss',
-})
-export class Reglas {}
--- frontend/src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://TU-BACKEND-URL.com/api/prode' // ← reemplazá con tu URL de producción
-};
